83d011d0de72d9a89bdb184085373d669e0277ee
Violet <violetavkhukova@gmail.com>
2019-07-07 19:05:14 +0100

    Initial site

---
+++ src/components/titleScreen.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default () => {
+    return (
+        <section className="intro">
+            <div className="inner">
+                <div className="upperContent">
+                    <h3>Violet + Thomas</h3>
+                </div>
+                <div className="content">
+                    <h1>We're getting married</h1>
+                    <div className="subTitle">
+                        October 19, 2019 / Red Rock Canyon / Las Vegas, Nevada
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import TitleScreen from "../components/titleScreen";
+
+export default () => (
+    <>
+        <TitleScreen />
+        {/* <p>More text here</p>
+        <p>And more here</p>
+        <p>And some more here</p> */}
+    </>
+);
